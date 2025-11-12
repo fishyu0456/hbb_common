989fd759ce1a847512e3c6552ca3bea4b0701728
fishyu0456 <2410035514@qq.com>
2025-11-12 16:20:16 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -105,7 +105,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["fishfish.space"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["fishfish.cc"];
 pub const RS_PUB_KEY: &str = "VOGZy7CePm9d7LLwbTvGabguvqIAnHqQSP+JOXiehXg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
